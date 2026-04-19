485700e2587e9136e648607ee6f1b9ff3009c666
Yandisa <gaju.yandisa@gmail.com>
2026-04-19 17:31:06 +0200

    Update settings.py

--- keyguy/settings.py
+++ keyguy/settings.py
@@ -61,7 +61,7 @@ WSGI_APPLICATION = 'keyguy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'data' / 'db.sqlite3',
     }
 }
 
@@ -96,7 +96,7 @@ EMAIL_PORT          = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS       = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_HOST_USER     = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL  = os.environ.get('DEFAULT_FROM_EMAIL', 'KeyGuy Boss D <keys@bossd.co.za>')
+DEFAULT_FROM_EMAIL  = os.environ.get('DEFAULT_FROM_EMAIL', 'KeyGuy Boss D <keyesdiagnosis@gmail.com>')
 
 # ── PRODUCTION SECURITY ───────────────────────────────────────────────────────
 if not DEBUG:
